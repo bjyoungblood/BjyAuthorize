9b6d17a2b7e0cb404e75aab7f4630fa8d8769c38
Adam Lundrigan <adamlundrigan@cdli.ca>
2012-07-25 15:08:25 -0230

    Update class names: Zend\Acl\* -> Zend\Permissions\Acl\*

--- src/BjyAuthorize/Provider/Role/ProviderInterface.php
+++ src/BjyAuthorize/Provider/Role/ProviderInterface.php
@@ -5,7 +5,7 @@ namespace BjyAuthorize\Provider\Role;
 interface ProviderInterface
 {
     /**
-     * @return array of \Zend\Acl\Role\RoleInterface
+     * @return array of \Zend\Permissions\Acl\Role\RoleInterface
      */
     public function getRoles();
 }
