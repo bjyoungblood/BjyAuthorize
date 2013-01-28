7b81156eb4dc75f95921398a812839a98f3f21eb
Tom Oram <tom@scl.co.uk>
2013-01-28 11:42:18 +0000

    Added a template global.php config file for setting up the doctrine paths for the module

--- config/module.config.php
+++ config/module.config.php
@@ -30,18 +30,4 @@ return array(
             'error/403' => __DIR__ . '/../view/error/403.phtml',
         ),
     ),
-    'doctrine' => array(
-        'driver' => array(
-            'bjyauthorize_entity' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
-                'paths' => __DIR__ . '/xml/doctrine-entities'
-            ),
-
-            'orm_default' => array(
-                'drivers' => array(
-                    'BjyAuthorize\Entity'  => 'bjyauthorize_entity'
-                )
-            )
-        )
-    ),
 );
---
+++ data/bjyauthorize-entity.global.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * BjyAuthorize Module (https://github.com/bjyoungblood/BjyAuthorize)
+ *
+ * @link https://github.com/bjyoungblood/BjyAuthorize for the canonical source repository
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+return array(
+    'doctrine' => array(
+        'driver' => array(
+            'bjyauthorize_entity' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
+                'paths' => __DIR__ . '/../../vendor/bjyoungblood/BjyAuthorize/config/xml/doctrine-entities',
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'BjyAuthorize\Entity'  => 'bjyauthorize_entity'
+                )
+            )
+        )
+    ),
+);
