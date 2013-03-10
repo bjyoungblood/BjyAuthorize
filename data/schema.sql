67f8b24344c4a19c6394b17ba095f24101f5f5df
Marco Pivetta <ocramius@gmail.com>
2013-03-10 19:00:55 +0100

    Renaming `default` to `is_default` in DDL

--- data/schema.sql
+++ data/schema.sql
@@ -1,14 +1,13 @@
-CREATE TABLE IF NOT EXISTS user_role (
-  role_id varchar(32) CHARACTER SET latin1 NOT NULL,
-  is_default tinyint(1) NOT NULL,
-  parent varchar(32) CHARACTER SET latin1  DEFAULT NULL,
-  PRIMARY KEY (role_id),
-  FOREIGN KEY parent_role (parent) REFERENCES user_role(role_id)
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `role_id` varchar(255) NOT NULL,
+  `is_default` tinyint(1) NOT NULL,
+  `parent` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS user_role_linker (
-  user_id int(11) unsigned NOT NULL,
-  role_id varchar(32) CHARACTER SET latin1 NOT NULL,
-  PRIMARY KEY (user_id, role_id),
-  FOREIGN KEY role (role_id) REFERENCES user_role(role_id)
+CREATE TABLE IF NOT EXISTS `user_role_linker` (
+  `user_id` int(11) unsigned NOT NULL,
+  `role_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `role_id` (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
