5adba22eabccf5e599765c87d68b722030e64450
Daniel Strøm <danielss89@gmail.com>
2013-07-03 13:37:49 +0200

    Update schema.sql
    Fixed mysql schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS `user_role` (
-  `role_id` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roleId` varchar(255) NOT NULL,
   `is_default` tinyint(1) NOT NULL,
-  `parent` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`role_id`)
+  `parent_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `user_role_linker` (
   `user_id` int(11) unsigned NOT NULL,
-  `role_id` varchar(255) NOT NULL,
+  `role_id` int(11) NOT NULL,
   PRIMARY KEY (`user_id`,`role_id`),
   KEY `role_id` (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
