195b36c783e8b8f3a259d065d0d166d8744990d9
Marco Pivetta <ocramius@gmail.com>
2013-07-26 19:34:37 +0200

    Aligning referenced field name to ZfcUser conventions

--- data/schema.sql
+++ data/schema.sql
@@ -5,16 +5,16 @@ CREATE  TABLE IF NOT EXISTS `user_role` (
   `parent_id` INT(11) NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `unique_role` (`role_id` ASC),
-  INDEX `parent_id_fk` (`parent_id` ASC),
-  CONSTRAINT `parent_id_fk` FOREIGN KEY (`parent_id`) REFERENCES `user_role` (`id`) ON DELETE SET NULL
+  INDEX `idx_parent_id` (`parent_id` ASC),
+  CONSTRAINT `fk_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `user_role` (`id`) ON DELETE SET NULL
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
 
 CREATE  TABLE IF NOT EXISTS `user_role_linker` (
   `user_id` INT(11) NOT NULL,
   `role_id` INT(11) NOT NULL,
   PRIMARY KEY (`user_id`, `role_id`),
-  INDEX `role_id_fk` (`role_id` ASC),
-  INDEX `user_id_fk` (`user_id` ASC),
-  CONSTRAINT `role_id_fk` FOREIGN KEY (`role_id`) REFERENCES `user_role` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+  INDEX `idx_role_id` (`role_id` ASC),
+  INDEX `idx_user_id` (`user_id` ASC),
+  CONSTRAINT `fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `user_role` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
