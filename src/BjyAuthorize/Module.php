b86ae8bfc77c990c1874f23c9d911c29f1d7a327
Valdas Petrulis <petrulis.valdas@gmail.com>
2014-05-21 00:53:09 +0300

    * Was impossible to inject 'unauthorized_strategy', because it was contructed ant bootstraping event; * I made more flexible, by registering 'unauthorized_strategy' just after EVENT_ROUTE;

--- src/BjyAuthorize/Module.php
+++ src/BjyAuthorize/Module.php
@@ -14,6 +14,7 @@ use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\ControllerPluginProviderInterface;
 use Zend\ModuleManager\Feature\ViewHelperProviderInterface;
+use Zend\Mvc\MvcEvent;
 use Zend\ServiceManager\AbstractPluginManager;
 
 /**
@@ -34,18 +35,11 @@ class Module implements
     public function onBootstrap(EventInterface $event)
     {
         /* @var $app \Zend\Mvc\ApplicationInterface */
-        $app            = $event->getTarget();
-        /* @var $sm \Zend\ServiceManager\ServiceLocatorInterface */
-        $serviceManager = $app->getServiceManager();
-        $config         = $serviceManager->get('BjyAuthorize\Config');
-        $strategy       = $serviceManager->get($config['unauthorized_strategy']);
-        $guards         = $serviceManager->get('BjyAuthorize\Guards');
+        $app = $event->getTarget();
+        $eventManager = $app->getEventManager();
 
-        foreach ($guards as $guard) {
-            $app->getEventManager()->attach($guard);
-        }
-
-        $app->getEventManager()->attach($strategy);
+        // Bind on event, then routing will be finished
+        $eventManager->attach(MvcEvent::EVENT_ROUTE, array($this, 'initBjyAuthorize'));
     }
 
     /**
@@ -56,12 +50,12 @@ class Module implements
         return array(
             'factories' => array(
                 'isAllowed' => function (AbstractPluginManager $pluginManager) {
-                    $serviceLocator = $pluginManager->getServiceLocator();
-                    /* @var $authorize \BjyAuthorize\Service\Authorize */
-                    $authorize = $serviceLocator->get('BjyAuthorize\Service\Authorize');
+                        $serviceLocator = $pluginManager->getServiceLocator();
+                        /* @var $authorize \BjyAuthorize\Service\Authorize */
+                        $authorize = $serviceLocator->get('BjyAuthorize\Service\Authorize');
 
-                    return new View\Helper\IsAllowed($authorize);
-                }
+                        return new View\Helper\IsAllowed($authorize);
+                    }
             ),
         );
     }
@@ -74,12 +68,12 @@ class Module implements
         return array(
             'factories' => array(
                 'isAllowed' => function (AbstractPluginManager $pluginManager) {
-                    $serviceLocator = $pluginManager->getServiceLocator();
-                    /* @var $authorize \BjyAuthorize\Service\Authorize */
-                    $authorize = $serviceLocator->get('BjyAuthorize\Service\Authorize');
+                        $serviceLocator = $pluginManager->getServiceLocator();
+                        /* @var $authorize \BjyAuthorize\Service\Authorize */
+                        $authorize = $serviceLocator->get('BjyAuthorize\Service\Authorize');
 
-                    return new Controller\Plugin\IsAllowed($authorize);
-                }
+                        return new Controller\Plugin\IsAllowed($authorize);
+                    }
             ),
         );
     }
@@ -104,5 +98,28 @@ class Module implements
     public function getConfig()
     {
         return include __DIR__ . '/../../config/module.config.php';
+    }
+
+    /**
+     * Initializes main functionality of BjyAuthorize module
+     *
+     * @param MvcEvent $e
+     */
+    public function initBjyAuthorize(MvcEvent $e)
+    {
+        $app = $e->getApplication();
+        /* @var $sm \Zend\ServiceManager\ServiceLocatorInterface */
+        $serviceManager = $app->getServiceManager();
+        $eventManager = $app->getEventManager();
+
+        $config         = $serviceManager->get('BjyAuthorize\Config');
+        $strategy       = $serviceManager->get($config['unauthorized_strategy']);
+        $guards         = $serviceManager->get('BjyAuthorize\Guards');
+
+        foreach ($guards as $guard) {
+            $eventManager->attach($guard);
+        }
+
+        $eventManager->attach($strategy);
     }
 }
