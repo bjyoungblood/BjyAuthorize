1ff277a734645a66e3695a1c9afa31e21917270a
Steve Rhoades <sedonami@gmail.com>
2014-07-26 12:31:54 -0700

    Support for a Cache Key Generator

---
+++ src/BjyAuthorize/Service/CacheKeyGeneratorFactory.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * BjyAuthorize Module (https://github.com/bjyoungblood/BjyAuthorize)
+ *
+ * @link https://github.com/bjyoungblood/BjyAuthorize for the canonical source repository
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace BjyAuthorize\Service;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Factory for building a cache key generator
+ *
+ * @author Steve Rhoades <sedonami@gmail.com>
+ */
+class CacheKeyGeneratorFactory implements FactoryInterface
+{
+    /**
+     * Create a cache key
+     *
+     * @param   ServiceLocatorInterface $serviceLocator
+     * @return  Callable
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = $serviceLocator->get('BjyAuthorize\Config');
+        $cacheKey = (!empty($config['cache_key'])) ? $config['cache_key'] : 'bjyauthorize_acl';
+
+        $callback = function() use ($cacheKey) {
+            return $cacheKey;
+        };
+
+        if(!empty($config['cache_key_generator']) && is_callable($config['cache_key_generator'])) {
+            $callback = $config['cache_key_generator'];
+        }
+
+        return $callback;
+    }
+}
